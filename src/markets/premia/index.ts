bd72d79153993c74f07a6e529e652f915b27ea8b
Michalis Kargakis <kargakis@protonmail.com>
2022-05-01 20:17:14 +0200

    Consolidate Premia-specific code in PremiaService

--- src/markets/premia/index.ts
+++ src/markets/premia/index.ts
@@ -1,23 +1,68 @@
+import { BigNumber, Contract, ethers, providers, utils, Wallet } from 'ethers'
+import { Contract as MulticallContract, Provider as MulticallProvider } from 'ethers-multicall'
+
 import ApiService from '../../services/api'
 import { config } from '../../config'
-import { fetchOptionsQuery, fetchPoolsQuery, fetchTokensQuery } from './queries'
+import { fetchOptionsQuery } from './queries'
 
 const subgraphURL = config.get('PREMIA_SUBGRAPH_API_URL')
 
-const PremiaService = {
-    async fetchTokens(): Promise<any> {
-        const resp = await ApiService.graphql(subgraphURL, fetchTokensQuery)
-        return resp.data
-    },
-    async fetchPools(): Promise<any> {
-        const resp =  await ApiService.graphql(subgraphURL, fetchPoolsQuery)
-        return resp.data
-    },
+class PremiaService {
+    // RPC providers
+    private provider: providers.StaticJsonRpcProvider
+    private multicallProvider: MulticallProvider
+
+    private wallet: Wallet
+
+    private linkPool: Contract
+
+    constructor() {
+        this.provider = new providers.StaticJsonRpcProvider(config.get('NODE_API_URL'))
+        this.multicallProvider = new MulticallProvider(this.provider)
+
+        this.wallet = new ethers.Wallet(config.get('PRIVATE_KEY'), this.provider)
+        console.log(`Keeper address: ${this.wallet.address}`)
+
+        const poolAbi = [
+            'function quote(address feePayer, uint64 maturity, int128 strike64x64, uint256 contractSize, bool isCall) external view returns (int128 baseCost64x64, int128 feeCost64x64, int128 cLevel64x64, int128 slippageCoefficient64x64)',
+        ]
+        this.linkPool = new Contract('0xf87Ca9EB60c2E40A6C5Ab14ca291934a95F845Ff', poolAbi, this.provider)
+    }
+
+    async fetchPremiums(): Promise<any> {
+        const options = await this.fetchOptions()
+        for (let o of options.data.options) {
+            if (o.pairName == 'LINK/DAI') {
+                console.log(o)
+                console.log(`maturity: ${new Date(o.maturity*1000)}`)
+
+                const [
+                    baseCost64x64,
+                    feeCost64x64,
+                    cLevel64x64,
+                    slippageCoefficient64x64
+                ] = await this.linkPool.quote(
+                    this.wallet.address,
+                    o.maturity,
+                    o.strike64x64,
+                    BigNumber.from('1000000000000000000000'),
+                    o.optionType == 'CALL'
+                )
+
+                console.log(`base cost: ${baseCost64x64}`)
+                console.log(`fee cost: ${feeCost64x64}`)
+                console.log(`C level: ${cLevel64x64}`)
+                console.log(`slippage: ${slippageCoefficient64x64}`)
+                break
+            }
+        }
+    }
+
     async fetchOptions(): Promise<any> {
         const now = Math.floor(Date.now() / 1000)
         const resp =  await ApiService.graphql(subgraphURL, fetchOptionsQuery(now))
         return resp.data
-    },
+    }
 }
 
 export default PremiaService
--- src/optionatoor.ts
+++ src/optionatoor.ts
@@ -1,6 +1,3 @@
-import { BigNumber, Contract, ethers, providers, utils, Wallet } from 'ethers'
-import { Contract as MulticallContract, Provider as MulticallProvider } from 'ethers-multicall'
-
 import { config } from './config'
 import PremiaService from './markets/premia'
 
@@ -8,26 +5,11 @@ export default class Optionatoor {
     // Whether the class is initialized
     private isInitialized: boolean = false
 
-    // RPC providers
-    private provider: providers.StaticJsonRpcProvider
-    private multicallProvider: MulticallProvider
-
-    private wallet: Wallet
-
-    // Premia pools
-    private premiaLinkPool: Contract
+    // AMMs
+    private premia: PremiaService
 
     constructor() {
-        this.provider = new providers.StaticJsonRpcProvider(config.get('NODE_API_URL'))
-        this.multicallProvider = new MulticallProvider(this.provider)
-
-        this.wallet = new ethers.Wallet(config.get('PRIVATE_KEY'), this.provider)
-        console.log(`Keeper address: ${this.wallet.address}`)
-
-        const poolAbi = [
-            'function quote(address feePayer, uint64 maturity, int128 strike64x64, uint256 contractSize, bool isCall) external view returns (int128 baseCost64x64, int128 feeCost64x64, int128 cLevel64x64, int128 slippageCoefficient64x64)',
-        ]
-        this.premiaLinkPool = new Contract('0xf87Ca9EB60c2E40A6C5Ab14ca291934a95F845Ff', poolAbi, this.provider)
+        this.premia = new PremiaService()
     }
 
     public async init(): Promise<void> {
@@ -37,29 +19,6 @@ export default class Optionatoor {
     public async run(): Promise<void> {
         if (!this.isInitialized) throw Error('uninitialized: did you run init()?')
 
-        const options = await PremiaService.fetchOptions()
-        for (let o of options.data.options) {
-            if (o.pairName == 'LINK/DAI') {
-                console.log(o)
-
-                const [
-                    baseCost64x64,
-                    feeCost64x64,
-                    cLevel64x64,
-                    slippageCoefficient64x64
-                ] = await this.premiaLinkPool.quote(
-                    this.wallet.address,
-                    o.maturity,
-                    o.strike64x64,
-                    BigNumber.from('10000000000000000'),
-                    o.optionType == 'CALL'
-                )
-
-                console.log(`base cost: ${baseCost64x64}`)
-                console.log(`fee cost: ${feeCost64x64}`)
-                console.log(`C level: ${cLevel64x64}`)
-                console.log(`slippage: ${slippageCoefficient64x64}`)
-            }
-        }
+        await this.premia.fetchPremiums()
     }
 }
