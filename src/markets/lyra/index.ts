b45c010b5f73e54aff8491963b112ed596fc49f2
Michalis Kargakis <kargakis@protonmail.com>
2022-05-05 18:29:30 +0200

    Add Lyra support

---
+++ src/markets/lyra/index.ts
@@ -0,0 +1,73 @@
+import Lyra from '@lyrafinance/lyra-js'
+import { BigNumber, utils } from 'ethers'
+
+import { IOption } from '../../types/option'
+
+class LyraService {
+    private lyra: Lyra
+
+    constructor() {
+        // const config = {
+        //     rpcUrl: 'https://mainnet.optimism.io/',
+        //     chainId: 420
+        // }
+        this.lyra = new Lyra()
+    }
+
+    fromBigNumber(number: BigNumber, decimals: number = 18): number {
+        return parseFloat(utils.formatUnits(number.toString(), decimals))
+    }
+
+    // Set isBuy to true to get quotes for long options
+    async getOptions(isBuy = false): Promise<IOption[]> {
+        const options: IOption[] = []
+
+        // Fetch all markets
+        const markets = await this.lyra.markets()
+        for (let market of markets) {
+            let contractSize: BigNumber
+            switch (market.name) {
+                case 'BTC':
+                    contractSize = utils.parseUnits('0.01')
+                    break
+                case 'ETH':
+                    contractSize = utils.parseUnits('0.1')
+                    break
+                default:
+                    throw new Error(`unknown market: ${market.name}`)
+            }
+
+            for (let board of market.liveBoards()) {
+                for (let strike of board.strikes()) {
+                    const call = await strike.quote(true, isBuy, contractSize)
+                    if (this.fromBigNumber(call.premium) != 0) {
+                        options.push({
+                            optionType: 'CALL',
+                            pairName: market.name,
+                            maturity: board.expiryTimestamp*1000,
+                            strike: strike.strikePrice,
+                            contractSize: utils.formatUnits(call.size),
+                            premium: call.premium, // Premium returned in sUSD
+                        })
+                    }
+
+                    const put = await strike.quote(false, isBuy, contractSize)
+                    if (this.fromBigNumber(put.premium) != 0) {
+                        options.push({
+                            optionType: 'PUT',
+                            pairName: market.name,
+                            maturity: board.expiryTimestamp*1000,
+                            strike: strike.strikePrice,
+                            contractSize: utils.formatUnits(put.size),
+                            premium: put.premium,  // Premium returned in sUSD
+                        })
+                    }
+                }
+            }
+        }
+
+        return options
+    }
+}
+
+export default LyraService
--- src/optionatoor.ts
+++ src/optionatoor.ts
@@ -1,19 +1,19 @@
 import { utils } from 'ethers'
 
-import { config } from './config'
+import LyraService from './markets/lyra'
 import PremiaService from './markets/premia'
 
 export default class Optionatoor {
     // Whether the class is initialized
     private isInitialized: boolean = false
 
-    private maxBuyUSD = 0
-
     // AMMs
     private premia: PremiaService
+    private lyra: LyraService
 
     constructor() {
         this.premia = new PremiaService()
+        this.lyra = new LyraService()
     }
 
     public async init(): Promise<void> {
@@ -24,6 +24,19 @@ export default class Optionatoor {
     public async run(): Promise<void> {
         if (!this.isInitialized) throw Error('uninitialized: did you run init()?')
 
+        console.log('Getting Lyra markets...')
+        const lyraOptions = await this.lyra.getOptions()
+        for (let o of lyraOptions) {
+            console.log(`Asset: ${o.pairName}`)
+            console.log(`Type: ${o.optionType}`)
+            console.log(`Maturity: ${new Date(o.maturity)}`)
+            console.log(`Strike: ${utils.formatUnits(o.strike)}`)
+            console.log(`Contract size: ${o.contractSize}`)
+            console.log(`Premium: ${utils.formatUnits(o.premium)}`)
+            console.log()
+        }
+
+        console.log('Getting Premia markets...')
         const premiaOptions = await this.premia.getOptions()
         for (let o of premiaOptions) {
             console.log(`Pair: ${o.pairName}`)
