e09142dc6ab2b22a481449d910899e5e53e02c3e
Michalis Kargakis <kargakis@protonmail.com>
2022-05-19 11:44:48 +0200

    Fetch both Lyra bids and asks

--- src/optionatoor.ts
+++ src/optionatoor.ts
@@ -24,9 +24,9 @@ export default class Optionatoor {
     public async run(): Promise<void> {
         if (!this.isInitialized) throw Error('uninitialized: did you run init()?')
 
-        console.log('Getting Lyra markets...')
-        const lyraOptions = await this.lyra.getOptions()
-        for (let o of lyraOptions) {
+        console.log('\x1b[1mGetting Lyra bids...\x1b[0m')
+        const lyraSellOptions = await this.lyra.getOptions()
+        for (let o of lyraSellOptions) {
             console.log(`Asset: ${o.pairName}`)
             console.log(`Type: ${o.optionType}`)
             console.log(`Maturity: ${new Date(o.maturity)}`)
@@ -36,7 +36,19 @@ export default class Optionatoor {
             console.log()
         }
 
-        console.log('Getting Premia markets...')
+        console.log('\x1b[1mGetting Lyra asks...\x1b[0m')
+        const lyraBuyOptions = await this.lyra.getOptions(true)
+        for (let o of lyraBuyOptions) {
+            console.log(`Asset: ${o.pairName}`)
+            console.log(`Type: ${o.optionType}`)
+            console.log(`Maturity: ${new Date(o.maturity)}`)
+            console.log(`Strike: ${utils.formatUnits(o.strike)}`)
+            console.log(`Contract size: ${o.contractSize}`)
+            console.log(`Premium: ${utils.formatUnits(o.premium)}`)
+            console.log()
+        }
+
+        console.log('\x1b[1mGetting Premia asks...\x1b[0m')
         const premiaOptions = await this.premia.getOptions()
         for (let o of premiaOptions) {
             console.log(`Pair: ${o.pairName}`)
